e59c83c04263c9522ce47249ef3f2d47cb056f13
jayasarikapathuri <jayasarikapathuri@DESKTOP-E3JD9SJ>
2019-12-02 17:31:29 +0530

    created setter and getter methods

---
+++ github/src/github/Sarika.java
@@ -0,0 +1,16 @@
+package github;
+
+public class Sarika {
+
+	private String sarika;
+
+	public String getSarika() {
+		return sarika;
+	}
+
+	public void setSarika(String sarika) {
+		this.sarika = sarika;
+	}
+	
+	
+}
